fa1a047353100ec912df098a7754cbfdb6911db7
suleymanacikgoz <62662812+suleymanacikgoz@users.noreply.github.com>
2021-02-03 23:47:23 +0300

    03.02.2021 ders sonu

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -1,4 +1,5 @@
 ﻿using Business.Concrete;
+using DataAccess.Concrete.EntitiyFreamwork;
 using DataAccess.Concrete.InMemory;
 using System;
 
@@ -8,8 +9,8 @@ namespace ConsoleUI
     {
         static void Main(string[] args)
         {
-            ProductManager productManager = new ProductManager(new InMemoryProductDal());
-            foreach (var item in productManager.GetAll())
+            ProductManager productManager = new ProductManager(new EfProductDal());
+            foreach (var item in productManager.GetByUnitPrice(50,100))
             {
                 Console.WriteLine(item.ProductName);
             }
---
+++ DataAccess/Concrete/EntitiyFreamwork/NorthWindContext.cs
@@ -0,0 +1,21 @@
+﻿using Entities.Concrete;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataAccess.Concrete.EntitiyFreamwork
+{
+    //Context : Db tabloları ile proje classlarını bağlamak
+    public class NorthWindContext:DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=(localdb)\mssqllocaldb;Database=Northwind;Trusted_Connection=true");
+        }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Customer> Customers { get; set; }
+
+    }
+}
